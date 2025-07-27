d1b92aacb50c79411681983b75991731c8aea8c2
Adarsh Sahu <adarshsahu2510@gmail.com>
2025-07-28 00:33:24 +0530

    fix

--- js/menu/menu.js
+++ js/menu/menu.js
@@ -966,6 +966,8 @@ ddsmoothmenu.init({
 	//customtheme: ["#1c5a80", "#18374a"],
 	contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
 })
+
+smoothmenu1
 ddsmoothmenu.init({
 	mainmenuid: "smoothmenu2", //menu DIV id
 	orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
